4ac2bdb20163a303a47a5b8791e0a06856db873a
Naygel22 <piotrkozlowski14@gmail.com>
2024-01-03 21:48:15 +0100

    click on question and mobile view

--- faq.js
+++ faq.js
@@ -1,5 +1,6 @@
 const questionBars = document.querySelectorAll('.questionBar');
 
+
 questionBars.forEach((questionBar) => {
   const plusImg = questionBar.querySelector('.plusImg');
   const minusImg = questionBar.querySelector('.minusImg');
@@ -9,20 +10,22 @@ questionBars.forEach((questionBar) => {
   answerText.classList.add('hidden');
   minusImg.classList.add('hidden');
 
-  function showAnswer() {
-    questionText.style.color = 'rgb(158,55,206)';
-    answerText.classList.remove('hidden');
-    plusImg.classList.add('hidden');
-    minusImg.classList.remove('hidden');
+  function toggleAnswer() {
+    if (answerText.classList.contains('hidden')) {
+      questionText.style.color = 'rgb(158, 55, 206)';
+      answerText.classList.remove('hidden');
+      plusImg.classList.add('hidden');
+      minusImg.classList.remove('hidden');
+    } else {
+      questionText.style.color = 'hsl(292, 42%, 14%)';
+      answerText.classList.add('hidden');
+      plusImg.classList.remove('hidden');
+      minusImg.classList.add('hidden');
+    }
   }
 
-  function hideAnswer() {
-    questionText.style.color = 'hsl(292, 42%, 14%)';
-    answerText.classList.add('hidden');
-    plusImg.classList.remove('hidden');
-    minusImg.classList.add('hidden');
-  }
-
-  plusImg.addEventListener('click', showAnswer);
-  minusImg.addEventListener('click', hideAnswer);
+  plusImg.addEventListener('click', toggleAnswer);
+  minusImg.addEventListener('click', toggleAnswer);
+  questionText.addEventListener('click', toggleAnswer);
+  
 });
